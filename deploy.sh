e18d948a2d6501b48e297f7124b0187e7b7a6aa5
Michael Magan <magan@indeed.com>
2017-09-15 18:16:35 -0400

    moved git command to the right directories

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Copy Build to Github Directory
 cp -r .build/. ../grouchycoterie.github.io
+cd ../grouchycoterie.github.io
+
+#Pull Master, Commit and & Merge
 git pull origin master
 git add .
 git commit -m '$1'
