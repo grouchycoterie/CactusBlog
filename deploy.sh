324a6ff6518df77bd7906ddca91826d334f32928
Michael Magan <michael@magan.info>
2017-12-26 13:53:14 +0000

    Added another blog post and removed unecessary pages.

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
+_current_directory="$(pwd)"
+
 if ! [ -d "../grouchycoterie.github.io" ]; then
   # Control will enter here if grouchycoterie.github.io exists.
   cd ../
   git clone git@github.com:grouchycoterie/grouchycoterie.github.iogit.git
-  cd CactusBlog
+  cd _current_directory
 fi
-
-if ! [ -d "~/.pyenv/versions/env-3.5.4" ]; then
-  # Create python virtual env
-  pyenv virtualenv 3.5.4 env-3.5.4
-fi
-
-#activate env and install requirements
-source activate env-3.5.4
-pip install --upgrade pip
-pip install -r requirements.txt
 
 # Copy Build to Github Directory
 cp -r .build/. ../grouchycoterie.github.io
